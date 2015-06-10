f722dcd4c83b3c2a9f360d7fa838447136f33902
Tushar Borole <tushariscoolster@gmail.com>
2015-06-10 13:56:59 +0530

    Cors added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var db = require('./models');
 var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./config/config.js'); // get our config file
-var authenticate = express.Router(); 
+var authenticate = express.Router();
 var jwt = require('jsonwebtoken');
 
 // Setting Parameters to server
@@ -22,63 +22,75 @@ var jwt = require('jsonwebtoken');
 //server.use(restify.bodyParser());
 var app = express();
 app.use(bodyParser.json());
-app.set('superSecret', config.secret); 
+app.set('superSecret', config.secret);
 
+//app.use(require('express-cors-options')(options));
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With, X-HTTP-Method-Override");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,HEAD,OPTIONS');
+    next();
+});
 
 
 // route middleware to verify a token
-authenticate.use(function(req, res, next) {
+authenticate.use(function (req, res, next) {
 
-  // check header or url parameters or post parameters for token
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-  // decode token
-  if (token) {
+    // decode token
+    if (token) {
 
-    // verifies secret and checks exp
-    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
-      if (err) {
-        return res.json({ success: false, message: 'Failed to authenticate token.' });    
-      } else {
-        // if everything is good, save to request for use in other routes
-        req.decoded = decoded;    
-        next();
-      }
-    });
+        // verifies secret and checks exp
+        jwt.verify(token, app.get('superSecret'), function (err, decoded) {
+            if (err) {
+                return res.json({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
 
-  } else {
+    } else {
 
-    // if there is no token
-    // return an error
-    return res.status(403).send({ 
-        success: false, 
-        message: 'No token provided.' 
-    });
-    
-  }
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
 });
 
 
 
 
-var routes =require('./controllers');
-require('./routes')(app,routes,authenticate);
+var routes = require('./controllers');
+require('./routes')(app, routes, authenticate);
 
 
 
 
- // Creating Tables or Initiating Connections
- db
-.sequelize
-.sync({ force: false})
-.nodeify(function(err) {
-  if (err) {
-    throw err;
-  } else {
-    // Listening in 8080 Port
-	app.listen(8080);
-	console.log("Server started: http://localhost:8080/");
+// Creating Tables or Initiating Connections
+db
+    .sequelize
+    .sync({
+        force: false
+    })
+    .nodeify(function (err) {
+        if (err) {
+            throw err;
+        } else {
+            // Listening in 8080 Port
+            app.listen(8080);
+            console.log("Server started: http://localhost:8080/");
 
-      
-  }
-});
+
+        }
+    });

d396c448c482d233e646c325507b00156e098c53
Tushar Borole <tushariscoolster@gmail.com>
2015-06-11 23:40:51 +0530

    route chages

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,7 +7,7 @@ module.exports = function (app, routes,authenticate) {
     app.post('/api/user', routes.user.createUser);
     app.post('/api/user', routes.user.createUser);
     app.post('/api/login',service.validation.validateRequest(validation.user.login), routes.user.login);
-    app.get('/auth/users',authenticate, routes.user.getAllUsers);
+    app.get('/api/users',authenticate, routes.user.getAllUsers);
 
 
 
